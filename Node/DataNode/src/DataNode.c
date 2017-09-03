2f9cd803e91ef110a87a24f45132ec87c630e080
jyp2712 <pompeypablo@hotmail.com>
2017-09-03 00:37:20 -0300

    Separo DataNode y Worker con sus configuraciones

---
+++ Node/DataNode/src/DataNode.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/config.h>
+
+typedef struct{
+    char* fs_ip;
+    char* fs_puerto;
+    char* nombre_datanode;
+    char* puerto_datanode;
+    char* ruta_databin;
+}t_dataNode;
+
+t_dataNode* config;
+
+t_dataNode *get_config(const char* path);
+
+int main() {
+    config = get_config("../../Configuracion");
+
+    return 0;
+}
+
+t_dataNode *get_config(const char *path) {
+	t_config* c = config_create((char *)path);
+	t_dataNode *config = malloc(sizeof(t_dataNode));
+
+	config->fs_ip = config_get_string_value(c, "FS_IP");
+	config->fs_puerto = config_get_string_value(c, "FS_PUERTO");
+	config->nombre_datanode = config_get_string_value(c, "NOMBRE_NODO");
+	config->puerto_datanode = config_get_string_value(c, "DATANODE_PUERTO");
+	config->ruta_databin = config_get_string_value(c, "RUTA_DATABIN");
+
+	return config;
+}
--- Node/Worker/src/Worker.c
+++ Node/Worker/src/Worker.c
@@ -5,31 +5,27 @@
 typedef struct{
     char* fs_ip;
     char* fs_puerto;
-    char* nombre_nodo;
     char* worker_puerto;
-    char* datanode_puerto;
     char* ruta_databin;
-}t_node;
+}t_worker;
 
-t_node* config;
+t_worker* config;
 
-t_node *get_config(const char* path);
+t_worker *get_config(const char* path);
 
 int main() {
-    config = get_config("../Configuracion");
+    config = get_config("../../Configuracion");
 
     return 0;
 }
 
-t_node *get_config(const char *path) {
+t_worker *get_config(const char *path) {
 	t_config* c = config_create((char *)path);
-	t_node *config = malloc(sizeof(t_node));
+	t_worker *config = malloc(sizeof(t_worker));
 
 	config->fs_ip = config_get_string_value(c, "FS_IP");
 	config->fs_puerto = config_get_string_value(c, "FS_PUERTO");
-	config->nombre_nodo = config_get_string_value(c, "NOMBRE_NODO");
 	config->worker_puerto = config_get_string_value(c, "WORKER_PUERTO");
-	config->datanode_puerto = config_get_string_value(c, "DATANODE_PUERTO");
 	config->ruta_databin = config_get_string_value(c, "RUTA_DATABIN");
 
 	return config;
