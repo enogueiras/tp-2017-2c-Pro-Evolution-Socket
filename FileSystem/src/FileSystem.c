be74e1e041ef4ee0ee09f4a2812c9242ab8ea75f
Emilio Jose Nogueiras <emilio.nogueiras@gmail.com>
2017-09-03 15:57:54 -0300

    Subo nuevamente la consola en FileSystem

--- FileSystem/src/FileSystem.c
+++ FileSystem/src/FileSystem.c
@@ -8,10 +8,33 @@
  ============================================================================
  */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+void consola(void);
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	consola();
 	return EXIT_SUCCESS;
+}
+
+void consola(void)
+{
+	char * linea;
+	  while(1) {
+	    linea = readline(">");
+	    if(linea)
+	      add_history(linea);
+	    if(!strncmp(linea, "exit", 4)) {
+	       free(linea);
+	       break;
+	    }
+	    printf("%s\n", linea);
+	    free(linea);
+	  }
 }
