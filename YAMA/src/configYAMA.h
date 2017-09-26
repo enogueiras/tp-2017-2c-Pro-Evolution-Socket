415b639ff0ba70c2bcba0e623bf2885ff60d8c63
julianroman <julianromane@gmail.com>
2017-09-26 00:06:23 -0300

    Separo archivo de configuracion YAMA

---
+++ YAMA/src/configYAMA.h
@@ -0,0 +1,25 @@
+/*
+ * configYAMA.h
+ *
+ *  Created on: 25/9/2017
+ *      Author: utnso
+ */
+
+#ifndef CONFIGYAMA_H_
+#define CONFIGYAMA_H_
+
+typedef struct {
+	char* fs_ip;
+	char* fs_puerto;
+	int retardo_planif;
+	char* algoritmo;
+	char* puerto_yama;
+	char* puerto_datanode;
+	char* ip_datanode;
+} t_yama;
+
+t_yama* config;
+
+t_yama *get_configYAMA(const char* path);
+
+#endif /* CONFIGYAMA_H_ */
